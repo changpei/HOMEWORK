16a2513f7d97e7a392c72da556895ca58bffcf09
changpei <915403545@qq.com>
2018-03-15 15:27:34 +0800

    write

--- CPool_Day03/ex_01.c
+++ CPool_Day03/ex_01.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
-
+#include <unistd.h>
 
 //Task 01
-int my_print_alpha(void){
-	int i;
-	for(i = 97;i <= 122; i++)
-	{
-		printf("%c", i); 
-	}
-	return 0;
-}
 
+int my_print_alpha(void){
+    int i;
+    for(i = 97;i <= 122; i++)
+    {
+        //printf("%c", i);
+        write(1, &i, 1);
+    }
+    return 0;
+}
+int main()
+{
+    my_print_alpha();
+}

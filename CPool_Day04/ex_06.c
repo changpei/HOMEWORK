e9cacea565261b3ddd49ea3367aaba2fe1759790
changpei <915403545@qq.com>
2018-03-23 20:05:34 +0800

    1

--- CPool_Day04/ex_06.c
+++ CPool_Day04/ex_06.c
@@ -1,33 +1,22 @@
+#include <unistd.h>
 #include <stdio.h>
 void my_sort_int_array ( int *array , int size )
 {
-    int i, j, tmp;
-    for(i=0;i<size-1;i++)
-    {
-        for(int j=i+1;j<size;j++)
-        {
-            if(array[i]>array[j])
-            {
-                tmp=array[i];
-                array[i]=array[j];
-                array[j]=tmp;
+    int i = 0,j = 0,temp;
+    
+    while(i < size - 1){
+        j = 0;
+        while(j < size - i - 1){
+            if(array[j] > array[j + 1]){
+                temp = array[j + 1];
+                array[j + 1] = array[j];
+                array[j] = temp;
             }
+            j++;
         }
-    }
-}
-
-
-int main(void)
-{
-    //task6
-    printf("\ntask6:\n");
-    int fff[]={2,9, 8, 1, 3, 6, 7};
-    my_sort_int_array(fff,7);
-    for(int i=0;i<7;i++)
-    {
-        printf("%d\n", fff[i]);
+        i++;
     }
     
-    return 0;
 }
+
 
--- CPool_Day08/my_strdup的副本.c
+++ CPool_Day08/my_strdup的副本.c
@@ -1,27 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
+void my_putchar(char);
 int my_putstr(char *);
-int my_strlen(char const*);
-char *my_strcat(char *,char const*);
-char *concat_params ( int argc , char **argv )
+
+int my_show_word_array( char * const *tab )
 {
-    int len = 0;
-    int i  = 0;
-    char *re = NULL;
-    while(i < argc){
-        len = len + my_strlen(argv[i]);
-        i++;
+    while(*tab){
+        my_putstr(*tab);
+        my_putchar('\n');
+        tab++;
     }
-    i = 0;
-    len = len + 5;
-    re = malloc(len);
-    while(i < argc){
-        re = my_strcat(re,argv[i]);
-        if(i < argc - 1)
-            re = my_strcat(re,"\n");
-        if(i == argc - 1)
-            re = my_strcat(re,"\0");
-        i++;
-    }
-    return re;
+    return 0;
 }
